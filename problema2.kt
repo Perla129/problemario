8eb368312657a153d4e89573d00d2512514f1123
Perla129 <L24230803@minatitlan.tecnm.mx>
2024-11-14 08:04:08 -0600

    problema2.kt

---
+++ problema2.kt
@@ -0,0 +1,9 @@
+//Calcular el promedio de tres numeros e imprimir el resultado
+
+fun main (){
+    var a = readln().toInt()
+    var b = readln().toInt()
+    var c = readln().toInt()
+    var promedio = ( a + b + c ) / 3
+    println(promedio)
+}
