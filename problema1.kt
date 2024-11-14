89b80b0c843c9839ec2b7aa76878d4752e853974
Perla129 <L24230803@minatitlan.tecnm.mx>
2024-11-14 07:56:41 -0600

    problema1.kt

---
+++ problema1.kt
@@ -0,0 +1,8 @@
+//Hacer un programa que sume dos numeros y muestre el resultado
+
+fun main (){
+    var a = readln().toInt()
+    var b = readln().toInt()
+    var suma = a + b
+    println(suma)
+}
